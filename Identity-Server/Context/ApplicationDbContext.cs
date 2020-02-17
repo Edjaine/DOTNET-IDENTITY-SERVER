2bcc0699b936f9896eb9f4ed28670f611aaf89d7
Edjaine Soares Oliveira <Edjaine Soares Oliveira>
2020-02-16 22:51:59 -0300

    Carga inicial

---
+++ Identity-Server/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using Identity_Server.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_Server.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder){
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ Identity-Server/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity_Server.Model
+{
+    public class ApplicationUser: IdentityUser
+    {
+        
+    }
+}
